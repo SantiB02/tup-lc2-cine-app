7ea82f40350c1fadac5167383d2552cce1c5e0b9
SantiB02 <santibrasca02@gmail.com>
2023-07-07 23:25:56 -0300

    Paginacion y arreglos API

--- js/common.js
+++ js/common.js
@@ -1,5 +1,10 @@
 const spinnerDiv = document.getElementById('spinnerDiv');
 
+function mostrarSpinner() {
+    contenedorPeliculas.innerHTML = ''; //vacío el contenedor de peliculas por si ya había pelis de otra página
+    spinnerDiv.style.display = 'flex';
+}
+
 function eliminarSpinner() {
     spinnerDiv.style.display = 'none';
 }
--- js/pages/index.js
+++ js/pages/index.js
@@ -1,7 +1,22 @@
-const divsPelicula = document.getElementsByClassName('pelicula'); //guardo el div de cada pelicula (4)
+const contenedorPeliculas = document.getElementById('contenedorPeliculas'); //guardo el contenedor de peliculas
 const inputCodigo = document.getElementById('codigoPeli'); //guardo el input para ingresar codigo pelicula
+const btnAnterior = document.getElementById('btnAnterior');
+let pagina = 1; //pagina en la que estoy (para paginacion)
 
-function requestCartelera() {
+function statusBtnAnterior(pag) {
+    if (pag > 1) {
+        btnAnterior.disabled = false; //habilito el boton Anterior (paginacion) si estoy en paginas siguientes a la primera
+        btnAnterior.classList.remove('btnDisabled');
+    } else if (pag == 1) {
+        btnAnterior.disabled = true; //si estoy en la primera página, deshabilito el boton Anterior (paginacion)
+        btnAnterior.classList.add('btnDisabled');
+        console.log('disable');
+    }
+}
+
+function requestCartelera(pagina = 1) { //por defecto es 1
+    statusBtnAnterior(pagina); //habilito o deshabilito el boton anterior
+
     const options = {
         method: 'GET',
         headers: {
@@ -10,13 +25,13 @@ function requestCartelera() {
         }
     };
 
-    fetch('https://api.themoviedb.org/3/movie/now_playing?language=es&page=1', options)
+    fetch('https://api.themoviedb.org/3/movie/now_playing?language=es&page=' + (pagina.toString()), options)
         .then(response => response.json())
         .then(response => {
             eliminarSpinner(); //elimino el mensaje de carga porque pude solicitar las pelis (ver common.js)
+            console.log(response);
 
-            for (let i = 0; i < 4; i++) {
-                //console.log(response); test para ver el objeto devuelto por la API
+            for (let i = 0; i < response.results.length; i++) {
                 pelicula = response.results[i]
                 let poster_path = pelicula.poster_path
                 let title = pelicula.title
@@ -25,7 +40,10 @@ function requestCartelera() {
                 let original_language = pelicula.original_language
                 let release_date = pelicula.release_date
 
-                divsPelicula[i].innerHTML = `
+                const divPelicula = document.createElement('div');
+                divPelicula.className = 'pelicula';
+
+                divPelicula.innerHTML = `
                 <img class="poster" src="https://image.tmdb.org/t/p/w500/${poster_path}">
                 <h3 class="titulo">${title}</h3>
                 <div class="desc-pelicula">
@@ -36,6 +54,7 @@ function requestCartelera() {
                 </div>
                 <button class="button" onclick="agregarFav_boton()">Agregar a Favoritos</button>
                 `;
+                contenedorPeliculas.appendChild(divPelicula); //le agrego el div con la pelicula al contenedor
             }
         })
         .catch(error => {
@@ -44,6 +63,22 @@ function requestCartelera() {
 }
 
 setTimeout(requestCartelera, 3000); //espera 3 segundos hasta hacer request a la API, para que llegue a mostrarse el spinner de carga
+
+function pasarPagina() {
+    if (pagina <= 1000) {
+        pagina += 1;
+        mostrarSpinner();
+        setTimeout(requestCartelera, 2000, pagina);
+    }
+}
+
+function volverPagina() {
+    if (pagina > 1) {
+        pagina -= 1;
+        mostrarSpinner();
+        setTimeout(requestCartelera, 2000, pagina);
+    }
+}
 
 function agregarFav_codigo() { //agrega una pelicula a Favoritos ingresando el codigo
     codigo = inputCodigo.value;
